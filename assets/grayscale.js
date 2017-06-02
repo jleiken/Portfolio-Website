25b7489d9a03b993e0dd365dfaac06f8d6dcf6be
jleiken <jacobleiken@gmail.com>
2017-06-02 16:30:22 -0700

    Mechanics fully working. Only personal info left

--- assets/grayscale.js
+++ assets/grayscale.js
@@ -2,6 +2,7 @@
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ * Modified by Jacob Leiken
  */
 
 // jQuery to collapse the navbar on scroll
@@ -17,17 +18,14 @@ $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
+// Also closes navbar
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+    	$(".navbar-collapse").collapse('hide');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $(".navbar-collapse").collapse('hide');
 });
