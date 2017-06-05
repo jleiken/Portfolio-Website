0089f9b05fa0ee156612f4c645b9a58920891957
jleiken <jacobleiken@gmail.com>
2017-06-05 14:20:06 -0700

    Added nice animations

--- assets/resume.js
+++ assets/resume.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 /*!
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
@@ -7,6 +8,7 @@
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
+	"use strict";
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -14,12 +16,14 @@ function collapseNavbar() {
     }
 }
 
+new WOW().init();
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 // Also closes navbar
 $(function() {
+	"use strict";
     $('a.page-scroll').bind('click', function(event) {
     	$(".navbar-collapse").collapse('hide');
         var $anchor = $(this);
